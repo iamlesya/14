7f749f379bfeaf8f53a3668129508aa2207655b2
iamlesya <Lesy8915@yandex.ru>
2023-04-08 17:02:11 +0300

    memory usage of vector

---
+++ libs/data_structures/vector/vector.h
@@ -0,0 +1,30 @@
+//
+// Created by Lesya on 08.04.2023.
+//
+
+#ifndef UNTITLED8_VECTOR_H
+#define UNTITLED8_VECTOR_H
+
+#include <ctype.h>
+#include <malloc.h>
+#include <stdio.h>
+#include <stdbool.h>
+
+
+typedef struct vector {
+    int *data; // указатель на элементы вектора
+    size_t size; // размер вектора
+    size_t capacity; // вместимость вектора
+} vector;
+
+vector createVector(size_t n);
+
+void reserve(vector *v, size_t newCapacity);
+
+void clear(vector *v);
+
+void shrinkToFit(vector *v);
+
+void deleteVector(vector *v);
+
+#endif //UNTITLED8_VECTOR_H
