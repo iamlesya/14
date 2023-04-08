f70a32adca17dddf2fdbee9151485fb630319814
iamlesya <Lesy8915@yandex.ru>
2023-04-08 17:25:38 +0300

    append push / pop functions

--- libs/data_structures/vector/vector.h
+++ libs/data_structures/vector/vector.h
@@ -27,4 +27,14 @@ void shrinkToFit(vector *v);
 
 void deleteVector(vector *v);
 
+bool isEmpty(vector *v);
+
+bool isFull(vector *v);
+
+int getVectorValue(vector *v, size_t i);
+
+void pushBack(vector *v, int x);
+
+void popBack(vector *v);
+
 #endif //UNTITLED8_VECTOR_H
